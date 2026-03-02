1b2043a0a95c5a6d2ee9a4c8af40c6e7caf8eade
GayFemboYchik <Zalupamamonta1488xyu@gmail.com>
2026-03-02 19:38:49 +0200

    Add files via upload

--- shop_kurtko_bain/instrument_shop/models.py
+++ shop_kurtko_bain/instrument_shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 class Category(models.Model):
@@ -19,19 +21,25 @@ class Instrument(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     in_stock = models.PositiveIntegerField()
     description = models.TextField()
+
     def __str__(self):
         return self.title
+
+
 class Order(models.Model):
     title = models.CharField(max_length=200)
     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE)
 
 
-
-
-class Client(models.Model):
+class Client(AbstractUser):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+
+
+    def __str__(self):
+        return self.username
 
 
 
--- shop_kurtko_bain/instrument_shop/views.py
+++ shop_kurtko_bain/instrument_shop/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import *
 
-# Create your views here.
+
+class InstrumentListView(ListView):
+    model = Instrument
+    template_name = "shop/instrument_list.html"
+    context_object_name = "instruments"
+
+    def get_queryset(self):
+        queryset = Instrument.objects.all()
+        subcategory_id = self.request.GET.get('subcategory')
+        category_id = self.request.GET.get('category')
+
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        if subcategory_id:
+            queryset = queryset.filter(subcategory_id=subcategory_id)
+
+        return queryset
+
