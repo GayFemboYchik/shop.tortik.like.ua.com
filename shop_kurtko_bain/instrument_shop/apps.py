4a7d4adbc0bfde4dc409a53d9b80b9a4a444224a
GayFemboYchik <Zalupamamonta1488xyu@gmail.com>
2026-03-06 20:27:07 +0200

    Add files via upload

--- shop_kurtko_bain/instrument_shop/apps.py
+++ shop_kurtko_bain/instrument_shop/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InstrumentShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instrument_shop'
+    name = 'shop'
--- shop_kurtko_bain/shop_kurtko_bain/settings.py
+++ shop_kurtko_bain/shop_kurtko_bain/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shop_kurtko_bain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
